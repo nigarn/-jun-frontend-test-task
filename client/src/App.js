d4b91552ec4104caff698f4d98c931c45bf2bb9d
Nigar Niftaliyeva <nigar.n@azerimed.local>
2022-08-11 12:23:01 +0400

    implement socket.io to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+
 import 'style/style.scss'
 
+const socket = io.connect("http://localhost:3002");
 
 function App() {
+  const [start, setStart] = useState(true);
+  const [stop, setStop] = useState("");
+  const [data, setData] = useState([]);
+
+  const startDistance = () => {
+    socket.emit("start", start);
+    console.log(start);
+    
+  };
+
+  useEffect(() => {
+    socket.on("ticker", (datas) => {
+      setData(datas);
+    });
+    console.log(data);
+  }, [data]);
+
   return (
     <div>
-      
+      <button onClick={startDistance}>Start</button>
     </div>
   );
 }
